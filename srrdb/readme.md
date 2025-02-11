99a0d92987452eeec5d354a2b6ac6afb76befb3e
Tanner Rogalsky <tanner.rogalsky@thescore.com>
2025-02-11 07:07:35 -0500

    initial commit

---
+++ srrdb/readme.md
@@ -0,0 +1,28 @@
+http://rescene.wikidot.com/
+https://api.srrdb.com/v1/docs
+
+r:The_Avengers_Battle_for_Earth_PAL_WiiU-VENOM (direct match of a releasename, faster than a normal search, used best in api)
+group:DIMENSION
+date:2025-02-07 (the date of which an srr was added to the db)
+nfo:yes | no
+srs:yes | no | broken
+foreign:yes | no (wheter the release is a foreign (non-english) release or not)
+confirmed:yes | no
+rarhash:e8455aaed5a92a24b10dad9d4802f4aeef04d38c (a unique string generated based on the set of rar-files in the release)
+category:unknown | tv | xvid | x264 | dvdr | xxx | pc | ngc | wii | 3ds | nds | xbox360 | xbox | ps3 | psp | complete+bluray | wmv | subpack | apps | game | flac | mp3 | mvid | wiiu | ps4 | xboxone | psv | nsw | gba | gbc | gizmondo | gp32 | ngage | ngp | tapwave+zodiac | ws | ebook | scene+notice | n64 | psx | ps2 | dc | cover | ogg | remux | ps5
+imdb:tt0289043 | 0289043 | 0000000
+start:xxx
+genre:action (etc...)
+language:burmese (etc...)
+country:kazakhstan (etc...)
+archive-crc:3D7C3182 (searches on the CRC of a file archived within the RARs)
+archive-size:xxx
+isdbhash:db7b7928f550789c (searches on ISDb (Internet Subtitles Database) hash)
+lower:yes (lists release names that are all lower case)
+endorstartwithdot:xxx
+firstupper:yes (some of these releases aren't badly named)
+compressed:yes | no (whether or not the archive was created using compression)
+skip:100 (skip over X releases in the result)
+skipr:200.10 (skip over X releases in the result and show Y results; limited to 50k.1k)
+store-real-filename:rfta-matrix-sample.avi (file name stored inside the srs)
+store-real-crc:b177cc14 (CRC32 of the complete sample)
---
+++ srrdb/src/lib.rs
@@ -0,0 +1,184 @@
+use futures::{FutureExt, TryFutureExt};
+pub use reqwest::Error as ReqwestError;
+
+#[derive(Debug)]
+pub enum Error {
+    NotFound,
+    Request(ReqwestError),
+}
+
+impl From<ReqwestError> for Error {
+    fn from(value: ReqwestError) -> Self {
+        Self::Request(value)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::NotFound => f.write_str("Error(NotFound)"),
+            Error::Request(error) => error.fmt(f),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+pub struct Client {
+    inner: reqwest::Client,
+    details_url: reqwest::Url,
+    download_url: reqwest::Url,
+}
+
+impl Client {
+    pub fn new() -> Self {
+        let inner = reqwest::Client::new();
+        let details_url = reqwest::Url::parse("https://api.srrdb.com/v1/details/").unwrap();
+        let download_url = reqwest::Url::parse("https://www.srrdb.com/download/file/").unwrap();
+        Self {
+            inner,
+            details_url,
+            download_url,
+        }
+    }
+
+    pub fn details_request<'a, R: Into<DetailsRequest<'a>>>(
+        &self,
+        request: R,
+    ) -> impl futures::Future<Output = Result<DetailsResponse, Error>> {
+        let request = request.into();
+        let url = self.details_url.join(&request.release_name).unwrap();
+        let request = self.inner.get(url).build().unwrap();
+        self.inner
+            .execute(request)
+            .and_then(|response| response.json::<DetailsOrNotFound>())
+            .err_into::<Error>()
+            .and_then(|response| {
+                futures::future::ready(match response {
+                    DetailsOrNotFound::NotFound(_) => Err(Error::NotFound),
+                    DetailsOrNotFound::Details(details_response) => Ok(details_response),
+                })
+            })
+    }
+
+    pub fn file_request(
+        &self,
+        request: FileRequest,
+    ) -> impl futures::Future<Output = Result<Vec<u8>, Error>> {
+        let input = format!("{}/{}", request.base, request.details.name);
+        let url = self.download_url.join(&input).unwrap();
+        let request = self.inner.get(url).build().unwrap();
+        self.inner
+            .execute(request)
+            .and_then(|response| response.bytes())
+            .map_ok(Into::into)
+            .err_into()
+    }
+}
+
+pub struct DetailsRequest<'a> {
+    pub release_name: std::borrow::Cow<'a, str>,
+}
+
+impl<'a> From<&'a str> for DetailsRequest<'a> {
+    fn from(release_name: &'a str) -> Self {
+        Self {
+            release_name: std::borrow::Cow::Borrowed(release_name),
+        }
+    }
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(untagged)]
+enum DetailsOrNotFound {
+    NotFound([(); 0]),
+    Details(DetailsResponse),
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct DetailsResponse {
+    pub name: String,
+    pub files: Vec<FileDetails>,
+    #[serde(rename = "archived-files")]
+    pub archived_files: Vec<FileDetails>,
+}
+
+impl DetailsResponse {
+    pub fn file_request(&self, name: &str) -> Option<FileRequest> {
+        self.files.iter().find_map(|details| {
+            (&details.name == name).then_some(FileRequest {
+                base: &self.name,
+                details,
+            })
+        })
+    }
+}
+
+pub struct FileRequest<'a> {
+    base: &'a str,
+    details: &'a FileDetails,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct FileDetails {
+    pub name: String,
+    pub size: u32,
+    // I think this is the u16 hex encoded
+    pub crc: String,
+}
+
+impl<'a> tower_service::Service<DetailsRequest<'a>> for Client {
+    type Response = DetailsResponse;
+    type Error = Error;
+    type Future = futures::future::BoxFuture<'a, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(
+        &mut self,
+        _cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        std::task::Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: DetailsRequest<'a>) -> Self::Future {
+        self.details_request(req).boxed()
+    }
+}
+
+impl<'a> tower_service::Service<FileRequest<'a>> for Client {
+    type Response = Vec<u8>;
+    type Error = Error;
+    type Future = futures::future::BoxFuture<'a, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(
+        &mut self,
+        _cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        std::task::Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: FileRequest<'a>) -> Self::Future {
+        self.file_request(req).boxed()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn it_works() {
+        let client = Client::new();
+        let response = client
+            .details_request(
+                "Harry.Potter.And.The.Chamber.Of.Secrets.2002.DVDRip.XViD-iNTERNAL-TDF",
+            )
+            .await
+            .unwrap();
+        let response = client
+            .file_request(response.file_request("tdf-hpatcos.nfo").unwrap())
+            .await
+            .unwrap();
+        let s = String::from_utf8_lossy(&response);
+        assert!(s.contains("Harry.Potter.And.The.Chamber.Of.Secrets.2002.DVDRip.XViD-iNTERNAL-TDF"));
+    }
+}
